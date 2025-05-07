ed2a892ca4d49825c170284569a463d6b8e51207
Спасскова Ксения <ksenia@MacBook-Pro-2.local>
2025-05-07 23:32:38 +0300

    added some translations

--- src/errorTranslator.tsx
+++ src/errorTranslator.tsx
@@ -1,9 +1,7 @@
 const errorMappings: Record<string, string> = {
-    'User exists': 'Пользователь с таким именем уже существует.',
-    'Invalid user credentials': 'Неверное имя пользователя или пароль.',
-    'Network Error': 'Проблемы с подключением. Проверьте интернет и повторите попытку.',
-    'Failed to create user': 'Не удалось создать пользователя. Проверьте данные и попробуйте снова.',
-    'invalid_grant': 'Неверные учетные данные. Повторите попытку.',
+    'Error while creating user': 'Ошибка создания клиента.',
+    'Invalid token': 'Попробуйте авторизоваться заново.',
+    'Both \'username\' and \'password\' must be provided': 'Заполните все поля.',
 };
 
 export const translateError = (error: any): string => {
